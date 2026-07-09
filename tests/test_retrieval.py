430ab05beeb755e9f817192d4daac16ce27fc2ee
masala-hadziavdic <amela188@hotmail.com>
2026-07-09 22:04:38 +0200

    Fix retrieval test and CI workflow

--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -1,7 +1,24 @@
+import faiss
+import pickle
+
 from rag.retrieval import retrieve
+
 
 def test_retrieve():
 
-    docs = retrieve("informatique")
+    index = faiss.read_index("data/index.faiss")
+
+    with open("data/chunks.pkl", "rb") as f:
+        chunks = pickle.load(f)
+
+    with open("data/metadata.pkl", "rb") as f:
+        metadata = pickle.load(f)
+
+    docs = retrieve(
+        "informatique",
+        index,
+        chunks,
+        metadata
+    )
 
     assert len(docs) > 0
